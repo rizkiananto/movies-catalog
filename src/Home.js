6878ee6f0628079d919a888d4cc56876674ed5a4
akbar rizkianto <akbarrizkianto@192.168.1.7>
2021-12-10 21:21:09 +0700

    installing axios, mui icon. navbar & search component created

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Navbar from './component/Navbar'
+import Search from './component/Search'
+import Box from '@mui/material/Box';
 
 function App() {
   return (
     <div className="App">
-        Home
+        <Navbar />
+        <Box 
+            sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '3rem'}} >
+            <Search />
+        </Box>
     </div>
   );
 }
