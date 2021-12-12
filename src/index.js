e0d98776545c273ae5dadfb581b79a3917c81188
akbar rizkianto <akbarrizkianto@akbars-MacBook-Air.local>
2021-12-12 17:58:30 +0700

    setup link route to favourite and home

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Home from './Home';
+import App from './Home';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+      <Router>
     <Provider store={store}>
-      <Home />
+        <App />
     </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
